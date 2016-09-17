3f9c2e0613ece1c63e5654c495bf7ad005b74de6
Herbert Henrique <eu@herberthenrique.com>
2016-09-17 11:49:32 -0300

    Fazendo calculo de investimento e mostrando tabela

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,12 +6,15 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController(treasuryService, indexesService) {
+  function MainController(treasuryService, indexesService, toastr) {
     var vm = this;
 
-    var treasuries = [];
-    var indexes = {};
+    vm.treasuries = [];
+    vm.indexes = {};
+    vm.calcDone = false;
     vm.model = {};
+
+    vm.calculate = calculate;
 
     activate();
 
@@ -22,14 +25,51 @@
 
     function getTreasuries() {
       treasuryService.getAll().then(function(data) {
-        treasuries = data;
+        vm.treasuries = data;
       });
     }
 
     function getIndexes() {
       indexesService.getAll().then(function(data) {
-        indexes = data;
+        vm.indexes = data;
+        vm.indexes.prefixado = 0;
       })
+    }
+
+    function calculate() {
+
+      var days = parseFloat(vm.model.days);
+      var investmentValue = parseFloat(vm.model.value);
+      if(!vm.model.value || investmentValue <= 0) {
+        toastr.warning('Preencha o valor de investimento');
+        return;
+      }
+
+      if(!vm.model.days || days <= 0) {
+        toastr.warning('Preencha o tempo de investimento');
+        return;
+      }
+
+      vm.investments = vm.treasuries.filter(function(t) {
+        return investmentValue >= t.minimumValue;
+      })
+      console.log('investments', vm.investments);
+
+      vm.investments = vm.investments.map(function(t) {
+        var investmentTax = t.currentInterestRate + vm.indexes[t.index.name];
+        var dayTax = investmentTax / 365;
+        var totalProfit = ((vm.model.days * dayTax) / 100) * vm.model.value;
+        t.profit = totalProfit.toFixed(2);
+        t.numberProfit = totalProfit;
+        t.total = (totalProfit + investmentValue).toFixed(2);
+        return t;
+      });
+
+      vm.investments.sort(function(a, b){
+        return a.numberProfit - b.numberProfit;
+      }).reverse();
+
+      vm.calcDone = true;
     }
 
   }

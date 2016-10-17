17604a074ba5671291f1b023ffe11ed9d8e671a1
Leandro Araujo <leandro.cesar.ma@gmail.com>
2016-10-17 01:33:12 -0400

    adicionando api ao whitelist

--- leandro_araujo/src/app/configurations/index.config.js
+++ leandro_araujo/src/app/configurations/index.config.js
@@ -6,9 +6,17 @@
     .config(config);
 
   /** @ngInject */
-  function config($logProvider) {
+  function config($logProvider, $sceDelegateProvider, $httpProvider) {
     // Enable log
     $logProvider.debugEnabled(true);
+
+    //$httpProvider.defaults.useXDomain = true;
+    //delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+    $sceDelegateProvider.resourceUrlWhitelist([
+      'self',
+      'https://private-anon-2dc5af8bab-ricocomvc.apiary-mock.com/**'
+    ]);
   }
 
 })();

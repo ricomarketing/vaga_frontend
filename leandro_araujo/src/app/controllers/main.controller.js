e0373a0707168f807dfe1b8336787f8240def2bb
Leandro Araújo <leandro.araujo@fpf.br>
2016-10-19 18:14:57 -0400

    ajustando view e calculo das taxas

--- leandro_araujo/src/app/controllers/main.controller.js
+++ leandro_araujo/src/app/controllers/main.controller.js
@@ -8,6 +8,7 @@
   /** @ngInject */
   function MainController(TreasuriesService, IndexesService, TAXAS, moment, momentBusiness, $q) {
     var vm = this;
+    vm.mostraView = false;
     vm.mostraTabela = false;
     vm.sliderConfigs = {
       minValue: 30,
@@ -26,37 +27,31 @@
     }
 
     $q.all([IndexesService.get(), TreasuriesService.get()]).then(function success(response) {
+      vm.mostraView = !vm.mostraView;
       vm.index      = response[0].data;
       vm.treasuries = response[1].data;
 
       criarLista(vm.treasuries, vm.index);
     });
 
-    // recebe indexes
-    /*IndexesService.get().then(function success(response) {
-      vm.index = response.data;
-    })
-
-    // recebe tesouros
-    TreasuriesService.get().then(function success(response) {
-      vm.treasuries = response.data;
-    })*/
-
     function criarLista(tesouros, index) {
-      console.log('quase');
-
       if( tesouros.length > 0 && Object.keys(vm.index) .length > 0 ) {
-        console.log('foi');
-
         tesouros.forEach( function(tesouro) {
           var dataLancamento    = moment(tesouro.issueDate, 'YYYY-MM-DD');
           var dataLimite        = moment(tesouro.maturityDate, 'YYYY-MM-DD');
           var tempoInvestimento = momentBusiness.weekDays( dataLancamento, dataLimite );
-
-          console.log(tempoInvestimento / 365);
+          var valorTaxa        = 0;
 
           // nominal a.a
-          var taxaInvestimento  = calcularTaxaInvestimento(tesouro.currentInterestPercentageValue, index.SELIC);
+          if(tesouro.index.name == 'IPCA') {
+            valorTaxa = index.IPCA;
+          if(tesouro.index.name == 'SELIC') {
+            valorTaxa = index.SELIC;
+          } else {
+            valorTaxa = 0;
+          }
+
+          var taxaInvestimento  = calcularTaxaInvestimento(tesouro.currentInterestPercentageValue, valorTaxa);
           var valorResgate      = calcularValorResgate(tesouro.minimumValue, taxaInvestimento, tempoInvestimento / 365);
 
           tesouro.rentabilidade = calcularRentabilidade(tesouro.minimumValue, valorResgate);

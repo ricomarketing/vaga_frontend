5437602e95ce8cc64b99a8a9d31c80c44a72cacd
Leandro Araújo <leandro.araujo@fpf.br>
2016-10-21 19:11:04 -0400

    removendo constantes

--- leandro_araujo/src/app/configurations/index.constants.js
+++ leandro_araujo/src/app/configurations/index.constants.js
@@ -4,8 +4,6 @@
 
   angular
     .module('rico')
-    .constant('moment', moment)
-    .constant('momentBusiness', momentBusiness)
     .constant('API', {
       'INDEXES': {
         'METHOD': 'GET',
@@ -15,10 +13,6 @@
         'METHOD': 'GET',
         'URL': 'https://private-anon-2dc5af8bab-ricocomvc.apiary-mock.com/treasury'
       }
-    })
-    .constant('TAXAS', {
-      'CUSTODIA': 0.003,
-      'CORRETORA': 0.001
     });
 
 })();
